b38e83f5051152258f894301c9ac493672d0a08e
Abel Lifaefi Mbula <abel@kinshasadigital.com>
2022-08-21 17:59:37 +0200

    feat: test set desktop background with Gio

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
+from gi.repository import Gio
 import os
 
-print(os.environ['DESKTOP_SESSION'])
+file_location = os.getcwd() + "/images/bkg.jpg"
+uri = 'file://%s' % file_location
+
+SCHEMA = "org.gnome.desktop.background"
+KEY = "picture-uri"
+gsettings = Gio.Settings.new(SCHEMA)
+gsettings.set_string(KEY, uri)
+
+# TODO
+# Load images from Wikimedia commons 1/day
+# Store images localy
+# Set background randomly
+# Make the program works for Windows
+# Automate the whole process
+
+gsettings.apply()
