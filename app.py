db5b0670274e536981cf8869a6ed6b9ff67114ba
Abel Lifaefi Mbula <abel@kinshasadigital.com>
2022-08-22 01:04:55 +0200

    feat: refact the script in order to be able to print current wallpaper and set one if image supplied

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
 from gi.repository import Gio
 import os
-
-file_location = os.getcwd() + "/images/bkg.jpg"
-uri = 'file://%s' % file_location
+from sys import argv
 
 SCHEMA = "org.gnome.desktop.background"
 KEY = "picture-uri"
+
 gsettings = Gio.Settings.new(SCHEMA)
-gsettings.set_string(KEY, uri)
+
+def get_wallpaper():
+    print(gsettings.get_string("picture-uri"))
+
+def set_wallpaper(image_path):
+    uri = 'file://%s' % image_path
+    gsettings.set_string(KEY, uri)
+
+def is_argument_free():
+    return len(argv) == 1
+
+if (is_argument_free == True):
+    get_wallpaper()
+else:
+    image_path = argv[1]
+    set_wallpaper(image_path)
 
 # TODO
 # Load images from Wikimedia commons 1/day
@@ -15,5 +29,4 @@ gsettings.set_string(KEY, uri)
 # Set background randomly
 # Make the program works for Windows
 # Automate the whole process
-
-gsettings.apply()
+# Apply background color if supplied
