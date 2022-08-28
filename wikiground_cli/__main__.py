f138415fa3b0e97706876040a5bf6661dbc067b8
Abel Lifaefi Mbula <abel@kinshasadigital.com>
2022-08-28 22:28:26 +0200

    fixbug: fix error that was preventing the program to run the --help command

--- wikiground_cli/__main__.py
+++ wikiground_cli/__main__.py
@@ -6,5 +6,5 @@ from wikiground_cli import cli, __app_name__
 def main():
     cli.app(prog_name=__app_name__)
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
